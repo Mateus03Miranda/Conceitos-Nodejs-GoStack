34a5806380e369863a3cbfd57e394216afa8f535
Mateus Miranda <mateusm.miranda80@gmail.com>
2020-08-22 21:07:26 -0300

    Conclusao do desafio

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+const { v4 } = require('uuid');
 
 const app = express();
 
@@ -10,24 +10,42 @@ app.use(cors());
 
 const repositories = [];
 
+function validateRepositoryId(request, response, next){
+  const {id}= request.params;
+  const repoIndex=repositories.findIndex(repository=>repository.id===id);
+  if(repoIndex<0)
+    return response.status("400").json("Repository not found");
+  request.repoIndex=repoIndex;
+  next();
+}
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title, url, techs}= request.body;
+  const repository={id:v4(), title, url, techs, likes:0};
+  repositories.push(repository);
+  return response.json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", validateRepositoryId,(request, response) => {
+  const {title, url, techs}= request.body;
+  repositories[request.repoIndex].title=title;
+  repositories[request.repoIndex].url=url;
+  repositories[request.repoIndex].techs=techs;
+  return response.json(repositories[request.repoIndex]);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id",validateRepositoryId,(request, response) => {
+  repositories.splice(request.repoIndex,1);
+  return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like",validateRepositoryId,(request, response) => {
+  repositories[request.repoIndex].likes++;
+  return response.send(repositories[request.repoIndex]);
 });
 
 module.exports = app;
